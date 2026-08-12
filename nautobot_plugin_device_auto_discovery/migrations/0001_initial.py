41844789780d9bcab6e45704afd589de8dceb989
Rawrroar <12380173+Rawrroar@users.noreply.github.com>
2026-08-13 08:56:21 +1200

    Fix migration: use string reference for PrimaryModel base class

--- nautobot_plugin_device_auto_discovery/migrations/0001_initial.py
+++ nautobot_plugin_device_auto_discovery/migrations/0001_initial.py
@@ -66,7 +66,7 @@ class Migration(migrations.Migration):
             options={
                 "ordering": ["-created"],
             },
-            bases=(nautobot.apps.models.PrimaryModel,),
+            bases=("nautobot.apps.models.PrimaryModel",),
         ),
         migrations.CreateModel(
             name="DiscoveryResult",
@@ -157,6 +157,6 @@ class Migration(migrations.Migration):
             options={
                 "ordering": ["-created"],
             },
-            bases=(nautobot.apps.models.PrimaryModel,),
+            bases=("nautobot.apps.models.PrimaryModel",),
         ),
     ]
